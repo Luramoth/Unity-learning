a5b708ef4dca51c20370fb14398f607cb171b05b
LuraMoth <fasreaperearnest@gmail.com>
2022-02-26 16:07:31 -0800

    add horizonal movement input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
 	public float vehicleSpeed = 20f;
 	public float turnSpeed;
 
+	public float hInput;
+
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -17,9 +19,12 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+		// get horizontal input usint he input class in unity which returns the variable going from 1 to 0 as a float variable
+		hInput = Input.GetAxis("Horizontal");
+
 		// fuckface hoo ha-ha
 		// jokes aside this is where we will have the vehicle start moving
 		transform.Translate(Vector3.forward * Time.deltaTime * vehicleSpeed);
-		transform.Translate(Vector3.right * Time.deltaTime* turnSpeed);
+		transform.Translate(Vector3.right * Time.deltaTime* turnSpeed * hInput);
 	}
 }
