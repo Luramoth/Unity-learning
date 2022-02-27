a5700cec04132354f0891922ada14c56c4188305
LuraMoth <fasreaperearnest@gmail.com>
2022-02-26 16:13:58 -0800

    add vertical control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
 	public float turnSpeed;
 
 	public float hInput;
+	public float vInput;
 
 	// Start is called before the first frame update
 	void Start()
@@ -19,12 +20,13 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		// get horizontal input usint he input class in unity which returns the variable going from 1 to 0 as a float variable
+		// get horizontal input using the input class in unity which returns the variable going from 1 to 0 as a float variable
 		hInput = Input.GetAxis("Horizontal");
+		vInput = Input.GetAxis("Vertical");
 
 		// fuckface hoo ha-ha
 		// jokes aside this is where we will have the vehicle start moving
-		transform.Translate(Vector3.forward * Time.deltaTime * vehicleSpeed);
+		transform.Translate(Vector3.forward * Time.deltaTime * vehicleSpeed * vInput);
 		transform.Translate(Vector3.right * Time.deltaTime* turnSpeed * hInput);
 	}
 }
